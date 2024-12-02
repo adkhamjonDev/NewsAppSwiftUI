79ec81b40a68a73a76ec37efc7757a4aa3ab952c
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-12-02 12:54:03 +0500

    Initial Commit

---
+++ NewsAppSwiftUI/NewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftUIApp.swift
+//  NewsAppSwiftUI
+//
+//  Created by Adkhamjon Rakhimov on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
